3003861b07499034166b18827fd02c15c5087fa3
Khanh Dang Kim Nguyen <khanh@trustingsocial.com>
2019-03-18 11:13:39 +1100

    add setup script for MacOS and Linux

---
+++ 1_setup_linux.sh
@@ -0,0 +1,4 @@
+wget https://repo.continuum.io/miniconda/Miniconda3-4.4.10-Linux-x86_64.sh
+chmod +x Miniconda3-4.4.10-Linux-x86_64.sh
+./Miniconda3-4.4.10-Linux-x86_64.sh -b -p ~/miniconda3
+~/miniconda3/bin/conda create --name label --file requirement.txt -y
---
+++ 2_run_mac_linux.sh
@@ -0,0 +1,1 @@
+source ~/miniconda3/bin/activate label && python annotate.py 1 1.0
